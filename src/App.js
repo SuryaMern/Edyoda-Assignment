13c13e17fa6c385f8ae8abe572d161f5464abdfe
SuryaMERN <krishsurya98@gmail.com>
2023-05-15 18:34:03 +0530

    Task'

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+import EdyodaHome from './EdyodaHome'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <EdyodaHome/>
+    </div>
+  )
+}
+
+export default App
